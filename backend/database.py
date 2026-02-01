da69570312cfa1421a78eef3fb2f51c73475c186
Sreedev S V <118549067+sree085@users.noreply.github.com>
2026-02-01 07:55:02 +0530

    Update MongoDB connection string

--- backend/database.py
+++ backend/database.py
@@ -3,11 +3,10 @@ import os
 
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://echogest:echogestPI5@cluster0.sjdebtx.mongodb.net/echogest?appName=Cluster0"
+    "mongodb+srv://echogest:<PASS>@cluster0.sjdebtx.mongodb.net/echogest?appName=Cluster0"
     )
     
-    # "mongodb+srv://echogest:echogestPI5@cluster0.xxxxx.mongodb.net/echogest?retryWrites=true&w=majority"
-#MONGO_URL = os.getenv("mongodb+srv://echogest:<echogestPI5>@cluster0.sjdebtx.mongodb.net/echogest?appName=Cluster0", "mongodb://localhost:27017")
+    # "mongodb+srv://echogest:<PASS>@cluster0.xxxxx.mongodb.net/echogest?retryWrites=true&w=majority"
 
 client = MongoClient(MONGO_URL)
 db = client["echogest"]
@@ -15,3 +14,4 @@ db = client["echogest"]
 gestures_col = db["gestures"]
 audio_col = db["audio_events"]
 devices_col = db["devices"]
+# echogestPI5
